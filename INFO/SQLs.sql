56cdbbbcfbf105c43c58bfa663decb3a5dc3fef7
Oleksiy Bechke <bechke@dlit.dp.ua>
2024-11-12 14:02:33 +0200

    Initial commit

---
+++ INFO/SQLs.sql
@@ -0,0 +1,11 @@
+
+CREATE DATABASE employee_db;
+
+CREATE TABLE IF NOT EXISTS employees
+( id BIGINT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  position VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
